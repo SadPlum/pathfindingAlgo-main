9fd135c9abf8eb39518cdfc9e7b80eff11afc0e3
SadPlum <sebastienolegault@gmail.com>
2023-01-31 18:15:33 -0400

    working on finding shortest path

--- search3.c
+++ search3.c
@@ -15,11 +15,19 @@
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         } ;
+    int safeGrid [10][10] ={};
     bool found = false;
     int farthestTotal=0;
     int farthestArray[10][2] = {};
     int oldTotal=1;
     int oldArray[10][2] = {};
+    int start[2];
+    int end[2];
+    // also used to determine shortest path length, (tempCounter +1) due to expand function being called once while outside loop;
+    int tempCounter = 0;
+    int pathIndex = 0;
+    bool pathFound = false;
+
 
 
 
@@ -34,8 +42,15 @@ bool outOfBounds(int y, int x) {
     return true;
 };
 
+bool onPath(int next) {
+    if(next == 2) {
+        return true;
+    }
+    return false;
+}
+
 int grow(int currentY,int currentX) {
-    printf("\nSTART Y: %d, X: %d\n\n", currentY, currentX);
+   
     if(found == true) { 
         return 0;
     };     
@@ -55,6 +70,8 @@ int grow(int currentY,int currentX) {
                 if(grid[y][x]==4) {
                     printf("found");
                     found = true;
+                    end[0] = y;
+                    end[1] = x;
                     return 0;
                 }
                 //  If passable (0), grid = true.
@@ -70,8 +87,83 @@ int grow(int currentY,int currentX) {
     return 0;
 };
 
-int recurse() {
+int establishRecur() {
 
+    // Establish safe copy of grid 
+
+  
+    for(int i =0; i< 10; i++) {
+        for (int j =0; j< 10; j++) {
+            safeGrid[i][j] = grid[i][j];
+        }
+    };
+
+    // Declare path array
+
+
+   
+    // FOR FUTURE
+    // int* direction() {
+    //     int y = start[0]; 
+    //     int x = start[1];
+    //     if(start[0] >= end[0]) {
+    //         if(start[1] >= end[1]) {
+    //             return int indexes[4][2] = {{y+1, x}, {y,x+1}, {y-1, x}, {y, x-1}};
+    //         };
+    //         if(start[1] <= end[1]) {
+    //             return int indexes[4][2] = {{y+1, x}, {y, x-1}, {y-1, x, {y, x+1}};
+
+    //         };
+    //     };
+    //     if(start[0] <= end[0]) {
+    //         if(start[1] >= end[1]) {
+    //             return int indexes[4][2] = {{y-1, x}, {y, x+1}, {y+1, x}, {y, x-1}};
+    //         };
+    //         if(start[1] <= end[1]) {
+    //             return int indexes[4][2] = {{y-1, x}, {y, x-1}, {y+1, x}, {y, x+1}};
+    //         };
+    //     }
+
+    //     return   int indexes[4][2] = {{y, x-1}, {y-1, x}, {y+1, x}, {y, x+1}};
+    //     }
+    // }
+
+    // int path* = direction();
+    int totalLength = tempCounter+1;
+    int pathArray[100][2]={};
+
+    int * array= recurse(start[0], start[1], pathIndex, totalLength, pathArray, pathFound);
+
+    for(int i=0; i<totalLength; i++) {
+        printf("%d, %d\n", array[i][0], array[i][1]);
+    }
+
+};
+
+
+int * recurse(int y, int x, int pathIndex, int length, int path[100][2], bool pathFound) {
+    int indexes[4][2] = {{y, x-1}, {y-1, x}, {y+1, x}, {y, x+1}};
+    for(int i = 0; i < 4; i++) {
+        if(pathIndex > length) {
+            return 0;
+        }
+        int newY = indexes[i][0];
+        int newX = indexes[i][1];
+        if(outOfBounds(newY, newX) == true) {
+            continue;
+        }
+        if(newY == end[0]) {
+            if(newX == end[1]) {
+                return path;
+            }
+        }
+        path[pathIndex][0] = newY;
+        path[pathIndex][1] = newX;
+        pathIndex++;
+        recurse(newY, newX, pathIndex, length, path[100][2], pathFound);
+
+    }
+    return;
 };
 
 
@@ -89,31 +181,34 @@ int expand() {
 }
 
 int search(int startY, int startX) {
-    oldArray[0][0] = startY;
-    oldArray[0][1] = startX;
-
-    expand();
-
-    int tempCounter = 0;
-
     while(tempCounter < 100) {
-    
-    printf("TEMP COUNTER %d\n", tempCounter);
-    
-    oldTotal = farthestTotal;
-   
-    for(int i = 0; i < farthestTotal; i++) {
-        oldArray[i][0] = farthestArray[i][0];
-        oldArray[i][1] = farthestArray[i][1];
-    };
-    farthestTotal= 0;
-    expand();
-    if(found == true) {
-        return 0;
+        // For first loop
+        if(tempCounter == 0) {
+            start[0] = startY;
+            start[1] = startX;
+            oldArray[0][0] = startY;
+            oldArray[0][1] = startX;
+            expand();
+            if(found == true) {
+                return 0;
+            }
+            tempCounter++;
+        }
+        // For recond loops
+        else {
+            oldTotal = farthestTotal;
+        for(int i = 0; i < farthestTotal; i++) {
+            oldArray[i][0] = farthestArray[i][0];
+            oldArray[i][1] = farthestArray[i][1];
+        };
+        farthestTotal= 0;
+        expand();
+        if(found == true) {
+            return 0;
+        }
+        tempCounter++;
+        }
     }
-    tempCounter++;
-    }
- 
     return 0;
 };
 
@@ -131,6 +226,9 @@ int main() {
     // SET END
 
     grid[0][0] = 4;
+
+
+    // SEARCH ALGO
 
     search(0,5);
     
@@ -161,6 +259,7 @@ int main() {
 
 
     // START RECURSION FOR SHORTEST PATH;
+    establishRecur();
 
     return 0;
 
